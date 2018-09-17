2142d71dfa31e67092514376f4b55308179f60b7
Oliver Pringle <ojapringle@gmail.com>
2018-09-17 08:02:56 +0200

    setup complete

---
+++ algorithsm.py
@@ -0,0 +1,7 @@
+"""
+Breadth-first search
+Depth-first search
+Binary search
+Merge sort
+Quick sort
+"""
---
+++ data_structures.py
@@ -0,0 +1,9 @@
+"""
+Linked lists
+Trees, Tries & Graphs
+Stacks & Queues
+Heaps
+Vectors
+Hash Tables
+"""
+
